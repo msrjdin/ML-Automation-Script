caaf83d7e7d0ee443a31c5ff59331d02616914cf
SirajuddinMohammed <msrjdin@gmail>
2020-06-19 18:07:45 +0530

    Column Type Confirmation Prompt Update

--- EDA/ColumnTypeIdentification.py
+++ EDA/ColumnTypeIdentification.py
@@ -1,4 +1,4 @@
-
+import inquirer
 
 class ColumnTypeIdentification:
     def __init__(self, df,y):
@@ -13,12 +13,11 @@ class ColumnTypeIdentification:
         
         #saving the final col type (Categorical, Text or Numeric)
         self.detecting_col_types()
-        target_type=''
-        for i in self.colTypes.keys():
-            if  self.y in self.colTypes[i]:
-                target_type=i
-                break
-        self.target_type=target_type
+        self.target_type=None
+
+        self.col_types_confirmation()
+
+
     
     
     #Categorical if the type is object and there are 20 distinct values in the first 95 %ile of the data else Text
@@ -30,16 +29,38 @@ class ColumnTypeIdentification:
             if self.dtypes[i]=='O':
                 if (self.df[i].fillna('',axis=0).apply(lambda x: len(x))).quantile(q=0.95)<20:
                     self.colTypes['Categorical'].append(i)
+                    self.dtypes[i]='Categorical'
                 #elif self.df[i].nunique() >= int(0.98*(self.df[i].shape[0])):
                     #self.colTypes['Identity'].append(i)
                 else:
                     self.colTypes['Text'].append(i)
+                    self.dtypes[i]='Text'
             else: 
                 distinctValues = self.df[i].nunique()
                 if distinctValues < int((self.df[i].shape[0])*0.05):
                     self.colTypes['Categorical'].append(i)
+                    self.dtypes[i] = 'Categorical'
                 #elif self.df[i].nunique() >= int(0.98 * (self.df[i].shape[0])):
                     #self.colTypes['Identity'].append(i)
                 else:
                     self.colTypes['Numeric'].append(i)
-    
+                    self.dtypes[i] = 'Numeric'
+
+    def col_types_confirmation(self):
+        columnTypesConfirmation=[]
+        print('Please Confirm/Map the columns with their type:\n')
+        for col in self.dtypes.keys():
+            colView=self.df[[col]].copy()
+            colView=colView[colView.col.isnotnull()].head(3).values
+            columnTypesConfirmation.append(
+                inquirer.List(col, message=str(col) + ':\nValues: {}'.format(colView),
+                                                         choices=['Text', 'Categorical', 'Numeric', 'Identity'],
+                                                         default=self.dtypes[col]))
+        confirmedColTypes=inquirer.prompt(columnTypesConfirmation)
+
+        self.colTypes = {'Categorical': [], 'Text': [], 'Numeric': [], 'Identity': []}
+        for col in self.dtypes.keys():
+            self.dtypes[col]=confirmedColTypes[col]
+            self.colTypes[confirmedColTypes[col]].append(col)
+            if self.y==col:
+                self.target_type=confirmedColTypes[col]
